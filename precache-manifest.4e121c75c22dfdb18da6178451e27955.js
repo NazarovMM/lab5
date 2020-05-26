6cebc04c0b0905ce5c997abfb5378e6388abafed
NazarovMM <maxzbox1@rambler.ru>
2020-05-26 13:30:41 +0300

    Updates

---
+++ precache-manifest.4e121c75c22dfdb18da6178451e27955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cc4aa879c1671edf7bafc9ba923381",
+    "url": "/lab5/index.html"
+  },
+  {
+    "revision": "cbea98b385bbda28fb15",
+    "url": "/lab5/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3b7c7a34a741abbc398e",
+    "url": "/lab5/static/js/2.c2c0c2f9.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/lab5/static/js/2.c2c0c2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbea98b385bbda28fb15",
+    "url": "/lab5/static/js/main.4d043019.chunk.js"
+  },
+  {
+    "revision": "b5889c70565772eebd8a",
+    "url": "/lab5/static/js/runtime-main.7f2cfe93.js"
+  },
+  {
+    "revision": "998116af174366584f81ba87a550115e",
+    "url": "/lab5/static/media/logo.998116af.png"
+  }
+]);
